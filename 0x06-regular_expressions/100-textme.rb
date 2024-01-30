cbc7bd5d4f93ecc1565f7ee17c69d8f7af101e69
AbuArwa001 <khalfanathman12@yahoo.com>
2024-01-31 02:44:10 +0300

    Extract kogs from file

---
+++ 0x06-regular_expressions/100-textme.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+#puts ARGV[0].match(/\[from:(\w+)\]\s\[to:([^w]?\w+)\]\s\[flags:([^w?]\d:[^w?]:[^w?]\d:\d:[^w]\d)\]/)
+match_data = ARGV[0].match(/\[from:([^w]?\w+)\]\s\[to:([^w]?\w+)\]\s\[flags:([^w]?\d:[^w]?\d*:[^w]?\d:[^w]?\d:[^w]?\d)\]/)
+
+if match_data
+  sender = match_data[1]
+  receiver = match_data[2]
+  flags = match_data[3]
+
+  puts "#{sender}, #{receiver}, #{flags}"
+end
