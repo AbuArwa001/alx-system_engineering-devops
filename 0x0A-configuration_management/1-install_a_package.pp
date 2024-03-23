043473ec8a8aac378260ac13ea99e444b6aa7290
AbuArwa001 <khalfanathman12@yahoo.com>
2024-03-23 08:39:53 +0300

    Install werksug dependacy for flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,11 @@
 # Install Flask version 
-require 'uri'
+package { 'Werkzeug':
+ensure   => '2.1.1',
+provider => 'pip3',
+}
 
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
-
-URI.encode_www_form_component('flask')
 
