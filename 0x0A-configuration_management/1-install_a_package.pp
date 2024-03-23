de8f45cab74a34f0167820c132c01af76d3a5b4b
AbuArwa001 <khalfanathman12@yahoo.com>
2024-03-23 04:43:16 +0300

    Install fask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,6 @@
 # Install Flask version 
-package { 'python3':
-  ensure => installed,
-}
-
-package { 'python3-pip':
-  ensure => installed,
-}
-
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
+
