18c64dd831452205919c3e5908d891742ea8153b
AbuArwa001 <khalfanathman12@yahoo.com>
2024-03-26 15:09:30 +0300

    Add puppet

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,5 +1,34 @@
-# Install Nginx web server (w/ Puppet)
-exec { 'server configuration':
-  provider => shell,
-  command  => 'sudo apt-get -y update; sudo apt-get -y install nginx; echo "Hello World!" > /var/www/html/index.html; sudo sed -i "/server_name _;/a location /redirect_me {\\n\\treturn 301 https://google.com; listen 80; \\n\\t}\\n" /etc/nginx/sites-available/default; sudo service nginx restart'
+# execute a command
+# Update apt packages
+exec { 'apt-update':
+  command => '/usr/bin/apt-get update',
+  path    => '/usr/bin',
+}
+
+# Install Nginx
+package { 'nginx':
+  ensure => 'latest',
+}
+
+# Allow Nginx through firewall
+exec { 'allow-nginx':
+  command => 'ufw allow "Nginx HTTP"',
+  path    => '/usr/bin',
+}
+
+# Serve "Hello World!" at the root
+file { '/var/www/html/index.nginx-debian.html':
+  content => 'Hello World!',
+}
+$replacement_str = "location /redirect_me {\n\treturn 301 https://www.youtube.com/;\n}"
+
+exec { 'redirect_page':
+  command => "sed -i '/^}/i ${replacement_str}' /etc/nginx/sites-available/default",
+  path    => '/usr/bin',
+}
+# Restart Nginx service
+service { 'nginx':
+  ensure  => 'running',
+  enable  => true,
+  require => Package['nginx'],
 }
