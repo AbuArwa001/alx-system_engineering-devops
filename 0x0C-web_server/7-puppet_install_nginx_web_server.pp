5a6421bc8730aa383bd63b6506f828128b630ddf
AbuArwa001 <khalfanathman12@yahoo.com>
2024-03-26 05:26:01 +0300

    Add configure server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -15,26 +15,6 @@ exec { 'allow-nginx':
   command => 'ufw allow "Nginx HTTP"',
   path    => '/usr/bin',
 }
-
-# Serve "Hello World!" at the root
-file { '/var/www/html/index.nginx-debian.html':
-  content => 'Hello World!',
-}
-
-# Configure 301 redirect for /redirect_me
-file { '/etc/nginx/sites-available/default':
-  ensure  => 'file',
-  content => "server {
-\tlisten 80;
-\tlisten [::]:80;
-\tserver_name _;
-\tlocation /redirect_me {
-\t\treturn 301 http://www.youtube.com/;
-\t}
-\t# Include the original configuration
-\tinclude /etc/nginx/sites-available/default;
-}",
-}
 # Restart Nginx service
 service { 'nginx':
   ensure  => 'running',
