519e3e38f2cf4603ff42e1274609512d86d17659
AbuArwa001 <khalfanathman12@yahoo.com>
2024-03-27 03:52:28 +0300

    SSH

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,19 @@
 #setup client config
-file { '/etc/ssh/ssh_config':
-  ensure  => file,
-  path    => '/etc/ssh/ssh_config',
-  mode    => '0744',
-  content => 'Host *
-    IdentityFile ~/.ssh/school
-    PasswordAuthentication no
-'
+
+include stdlib
+
+file_line { 'SSH Private Key':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    IdentityFile ~/.ssh/school',
+  match              => '^[#]+[\s]*(?i)IdentityFile[\s]+~/.ssh/id_rsa$',
+  replace            => true,
+  append_on_no_match => true
+}
+
+file_line { 'Deny Password Auth':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    PasswordAuthentication no',
+  match              => '^[#]+[\s]*(?i)PasswordAuthentication[\s]+(yes|no)$',
+  replace            => true,
+  append_on_no_match => true
 }
