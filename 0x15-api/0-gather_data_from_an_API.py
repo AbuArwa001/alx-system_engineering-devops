613a110ba30ce6589b9a09ee0fbfe463fec14e27
AbuArwa001 <khalfanathman12@yahoo.com>
2024-04-22 14:35:12 +0300

    Pycodestyle

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -21,14 +21,15 @@ def run():
     loded = json.loads(todos)
     EMPLOYEE = json.loads(user)
     no_d_tsks = sum(1 for i in loded if i['completed']
-                               and i['userId'] == int(sys.argv[1]))
+                    and i['userId'] == int(sys.argv[1]))
     ttl_tsk = sum(1 for i in loded
-                               if i['userId'] == int(sys.argv[1]))
+                  if i['userId'] == int(sys.argv[1]))
     for key, val in EMPLOYEE.items():
         if key == 'name':
             emn = val
             break
-    output = "Employee {} is done with tasks({}/{}):".format(emn, no_d_tsks, ttl_tsk)
+    output = "Employee {} is done with tasks({}/{}):"\
+        .format(emn, no_d_tsks, ttl_tsk)
     print(output)
     for i in loded:
         if i['completed'] and i['userId'] == int(sys.argv[1]):
