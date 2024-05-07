f9b7b454b6da1a2e63d7178135a548aecc770e28
AbuArwa001 <khalfanathman12@yahoo.com>
2024-05-08 00:48:17 +0300

    Reformat string

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,22 +7,8 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Gets the total subscribers for a given subreddit"""
-    CLIENT_ID = "lfFWjgI5ETUXf6vXN7F-lg"
-    SECRET_KEY = "M73OJpKH9_dS6r3GTQx5OKec5NCIZw"
-    auth = requests.auth.HTTPBasicAuth(CLIENT_ID, SECRET_KEY)
-    with open('pw.txt', 'r') as f:
-        pw = f.read()
-    data = {
-        'grant_type': 'password',
-        'username': 'khalfanathman',
-        'password': pw
-    }
-    headers = {"User-Agent": "Subs/0.1"}
-    res = requests.post('https://www.reddit.com/api/v1/access_token',
-                        auth=auth, data=data, headers=headers)
-    TOKEN = res.json()['access_token']
-    headers['Authorization'] = 'beare {}'.format(TOKEN)
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Subs/0.1"}
     response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
