3c44aed6d7824dc6329aaad1a6f7546f13fd6407
AbuArwa001 <khalfanathman12@yahoo.com>
2024-05-08 01:19:13 +0300

    A haash bang

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,20 +1,27 @@
+#!/usr/bin/python3
+import requests
 """
 Prints the titles of the first 10 hot posts listed for a given subreddit.
 """
-import requests
 
 
 def top_ten(subreddit):
-    """Gets the total subscribers for a given subreddit"""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {"User-Agent": "Subs/0.1"}
-    response = requests.get(url, headers=headers)
+    """
+    Prints the titles of the first 10 hot posts of a given subreddit.
+    """
+    # Reddit API URL for hot posts in a subreddit
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
+    headers = {'User-Agent': 'python:requests (by /u/khalfanathman)'}
 
+    # Make the request to the Reddit API
+    response = requests.get(url, headers=headers)
     if response.status_code == 200:
         data = response.json()
-        titles = data["data"]["children"]
-        for title in titles:
-            if "title" in title["data"]:
-                print(title["data"]["title"])
+        posts = data.get('data', {}).get('children', [])
+        if not posts:
+            print(None)
+        else:
+            for post in posts:
+                print(post['data']['title'])
     else:
         print(None)
