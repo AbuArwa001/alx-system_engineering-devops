4f11ca373e3f5d65b6b731ecdcf9047e32bf6232
AbuArwa001 <khalfanathman12@yahoo.com>
2024-05-09 14:50:15 +0300

    Hot ten tittles

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,10 +9,9 @@ def top_ten(subreddit):
     """Gets the total subscribers for a given subreddit"""
     req = requests.get(
         "https://www.reddit.com/r/{}/hot.json".format(subreddit),
-        headers = {'User-Agent': 'script:scr ipt:v1.0 (by /u/yourusername)'},
+        headers={'User-Agent': 'script:scr ipt:v1.0 (by /u/khalfanathman)'},
         params={"limit": 10},
     )
-    print(req.status_code)
     if req.status_code == 200:
         for get_data in req.json().get("data").get("children"):
             dat = get_data.get("data")
